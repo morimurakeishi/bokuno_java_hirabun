3d29a360731ce4fe2b8fde6667f4255c796e75f7
keishi morimura <sunshinemorimura777@gmail.com>
2026-04-15 19:00:22 +0900

    4章途中

---
+++ PoisonMatango.java
@@ -0,0 +1,19 @@
+public class PoisonMatango extends Matango {
+    int PoisonCout = 5;
+    public PoisonMatango(char suffix) {
+    super(suffix);
+    
+    }
+    
+    public void attack(Hero h) {
+        super.attack(h);
+        
+        if(this.PoisonCout>0) {
+          System.out.println("さらに毒の胞子をばらまいた");  
+          int dmg = h.hp / 5;
+          h.hp -= dmg;
+          System.out.println(dmg + "ポイントのダメージ");  
+          this.PoisonCout--;
+        }
+    }
+}
